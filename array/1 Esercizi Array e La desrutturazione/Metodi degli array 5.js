c5216d25b942e19abf2c61c13cb6b658b59ebdca
Klakkk <146862118+Klakkk@users.noreply.github.com>
2023-11-11 01:03:01 +0100

    Add files via upload

---
+++ array/1 Esercizi Array e La desrutturazione/Metodi degli array 5.js
@@ -0,0 +1,25 @@
+//Implementare la funzione ageAverage che, dato un array di persone, calcola l'età media.
+
+
+function calculateAverageAge(persons) {
+  const average= persons.reduce((a,user) => a+ user.age,0);  
+  const avg = average / persons.length;
+  return Math.round(avg);
+};
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const average = calculateAverageAge(persons);
+console.log(persons);
+console.log(average);
---
+++ array/2 Esercizi Template Strings/Template Strings 2.js
@@ -0,0 +1,5 @@
+//Utilizza una template string per creare una stringa su più righe che includa interruzioni di riga.
+
+const myString= `questa è stringa multilina
+semplificato, ovvero, usando il backstick non ho bisogno di scrivere\n\ per
+andare a capo`
