272fe29470cadeb2eac58a102dab37fac66cc78b
Klakkk <146862118+Klakkk@users.noreply.github.com>
2024-03-15 22:26:15 +0100

    Add files via upload

---
+++ uploadFiles/exercise.js
@@ -0,0 +1,12 @@
+function printName() {
+    console.log(true);
+}
+console.log("ole")
+printName();
+
+let= funzion = true ? "sesso" : "droga";
+
+
+if(funcitonprintName()= true){
+    console.log("sdrogo")
+}
