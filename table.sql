cc60c6f7fc4a896493e169c02b65697005119a9f
Klakkk <146862118+Klakkk@users.noreply.github.com>
2024-03-15 16:55:55 +0100

    table

---
+++ table.sql
@@ -0,0 +1,12 @@
+create table books (
+    id integer primary key,
+    book_id text id,
+    title text title,
+    author text author,
+    genre text genre,
+    published_year text,
+    isbn text isbn,
+    price number "",
+    rating number ,
+    stock_count number ""
+)
