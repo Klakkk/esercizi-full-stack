f6a4532fd02ff813cf1391be419aa1eca5bf5e33
Klakkk <146862118+Klakkk@users.noreply.github.com>
2024-03-15 22:28:46 +0100

    Add files via upload

---
+++ jvt1/start.js
@@ -0,0 +1,6 @@
+function cheRoba(name) { //qui indico sayHelloName 
+    return "se giochi Aatrox hai la mamma simpaticamente" // con return indico cosa indica cheRoba
+}
+ 
+let printName = cheRoba ("troia") // con let, indico la variante è cheRoba, con aggiunta la stringa che indicato "troia"
+     console.log(printName)  // eseguo la variante printName che è la funzione cheRoba con aggiunta la stringa
