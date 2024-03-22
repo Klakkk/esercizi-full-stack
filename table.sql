d5600e76382f7194d8a17b6e20e3272ec2eca9b1
Klakkk <146862118+Klakkk@users.noreply.github.com>
2024-03-22 15:33:42 +0100

    table.sql

--- table.sql
+++ table.sql
@@ -1,12 +1,11 @@
-create table books (
-    id integer primary key,
-    book_id text id,
-    title text title,
-    author text author,
-    genre text genre,
-    published_year text,
-    isbn text isbn,
-    price number "",
-    rating number ,
-    stock_count number ""
-)
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    genre VARCHAR(50),
+    published_year INT,
+    isbn VARCHAR(20),
+    price DECIMAL(10, 2),
+    rating DECIMAL(3, 2),
+    stock_count INT
+);
